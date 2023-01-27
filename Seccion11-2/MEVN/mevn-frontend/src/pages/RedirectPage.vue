151f3fad848d1f3659231d14e89e3b029601a1d0
AaronIME <aaron.isaac.echavarria@gmail.com>
2023-01-26 19:03:20 -0600

    Cambios deploy

---
+++ Seccion11-2/MEVN/mevn-frontend/src/pages/RedirectPage.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+    <q-page class="text-center q-pt-xl">
+        <h4>Te estamos redirigiendo...</h4>
+        <q-spinner-box color="primary" size="2em" />
+    </q-page>
+</template>
