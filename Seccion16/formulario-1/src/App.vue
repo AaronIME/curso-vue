7f8c0baafcc8cde2239f0f929421efee2bf557ed
AaronIME <aaron.isaac.echavarria@gmail.com>
2023-01-30 12:12:11 -0600

    Vue formularioas V-MODEL

---
+++ Seccion16/formulario-1/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import NavBar from './components/NavBar.vue';
+export default {
+  components:{
+    NavBar
+  }
+}
+</script>
+
+<template>
+  <NavBar></NavBar>
+  <div class="container">
+    <router-view/>  
+  </div>
+  
+</template>
+
+<style>
+
+</style>
---
+++ Seccion16/formulario-1/src/views/HomeView.vue
@@ -0,0 +1,78 @@
+<template>
+  <form @submit.prevent="procesarFormulario()">
+
+    <input type="text" class="form-control my-2" placeholder="Ingrese nombre" v-model.trim="tarea.nombre">
+
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="checkbox" id="check-1" v-model="tarea.categorias" value="javascript">
+      <label class="form-check-label" for="check-1">Javascript</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="checkbox" id="check-2" v-model="tarea.categorias" value="nodejs">
+      <label class="form-check-label" for="check-2">Node.js</label>
+    </div>
+
+    <div class="mt-2">
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id="radio-1" v-model="tarea.estado" value="urgente">
+        <label class="form-check-label" for="radio-1">Urgente</label>
+      </div>
+
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id="radio-1" v-model="tarea.estado" value="relax">
+        <label class="form-check-label" for="radio-1">Relax</label>
+      </div>
+    </div>
+
+    <div class="mt-2">
+      <input class="form-control" type="number" v-model.number="tarea.number">
+    </div>
+
+    <button class="btn btn-dark mt-5 btn-block" :disabled="bloquear" type="submit">Procesar</button>
+  </form>
+  <p>{{ tarea }}</p>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  },
+  data() {
+    return {
+      tarea: {
+        nombre: "",
+        categorias: [],
+        estado: "",
+        number: 0
+      }
+    }
+  },
+  methods: {
+    procesarFormulario() {
+      console.log(this.tarea);
+      if (this.tarea.nombre.trim() == "") {
+        console.log("Campo vacio");
+        return
+      }
+      console.log("No esta vacio");
+      this.tarea =
+      {
+        nombre: "",
+        categorias: [],
+        estado: "",
+        number: 0
+      }
+    }
+  },
+  computed: {
+    bloquear() {
+      return this.tarea.nombre.trim() != '' ? false : true;
+    }
+  }
+}
+</script>
